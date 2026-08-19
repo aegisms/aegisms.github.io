efbe16d5d3257d3f2625b8de9113b344b73e1edc
aegisms <aegisms2005@gmail.com>
2026-08-19 10:00:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+function showGreeting() {
+  const hour = new Date().getHours();
+  let message;
 
+  if (hour < 12) {
+    message = "Good Morning!";
+  } else if (hour < 18) {
+    message = "Good Afternoon!";
+  } else {
+    message = "Good Evening!";
+  }
+
+  const greetingEl = document.getElementById("greeting");
+  if (greetingEl) {
+    greetingEl.textContent = message;
+  }
+}
+
+showGreeting();
